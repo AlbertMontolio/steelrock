e8c9bfad9e0836a876c28251d9b7fe7689edfa26
Albert Montolio <albertmontolio@Alberts-MacBook-Pro.local>
2020-12-05 14:10:01 +0100

    d

--- src/pages/home/banner/Banner.jsx
+++ src/pages/home/banner/Banner.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 import { FormattedMessage } from 'react-intl'
+import { useMediaQuery } from 'react-responsive'
 
 import SolarImg from '../../../images/banner.jpg'
 
 const StyledBanner = styled.div`
-  background-image: url(${SolarImg});
   background-size: cover;
+  background-image: linear-gradient(rgba(0,0,0,.1),rgba(0,0,0,0.8)), url(${SolarImg});
+  background-image: linear-gradient(rgba(255,255,255,.1),rgba(255,255,255,1)), url(${SolarImg});
   width: 100%;
   height: calc(90vh - 60px);
   display: flex;
@@ -15,15 +17,20 @@ const StyledBanner = styled.div`
 `
 
 const StyledText = styled.div`
-  font-size: 40px;
+  font-size: ${({isDesktopOrLaptop}) => isDesktopOrLaptop ? '40px' : '30px'};
   font-weight: bold;
   color: #083D77;
+  padding: 0px ${({isDesktopOrLaptop}) => isDesktopOrLaptop ? '100px' : '20px'};
 `
 
 export const Banner = () => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1224px)'
+  })
+
   return (
     <StyledBanner>
-      <StyledText>
+      <StyledText isDesktopOrLaptop={isDesktopOrLaptop}>
         <FormattedMessage id = "home.banner" />
       </StyledText>
     </StyledBanner>
